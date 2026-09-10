8176d717bcdc5770abf5447f5a95069aa0df2614
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2026-09-10 00:56:30 -0400

    feat: random task names

--- src/lib/rng.new.ts
+++ src/lib/rng.new.ts
@@ -2,6 +2,7 @@ export function rollDie(sides: number) {
   return randomInt(1, sides)
 }
 
+// TODO: replace with proper PRNG
 export function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
--- src/lib/task.new.ts
+++ src/lib/task.new.ts
@@ -7,13 +7,20 @@ export function newTask(type?: TaskType) {
   const difficulty = taskType === TASK_TYPE.dungeon ? randomInt(2, 10) : null
   return {
     id: crypto.randomUUID() as string,
-    name: `A ${taskType}${skill ? ` ${skill}` : ""} task`,
+    name: taskName(taskType, skill ?? "", difficulty ?? 0),
     type: taskType,
     maxProgress: randomInt(5, 10),
     progress: 0, // TODO: move to new Assignment object, maybe on hero
     skill,
     difficulty,
   }
+}
+
+function taskName(type: TaskType, skill: string, difficulty: number) {
+  // previously, was
+  // return `A ${type} ${skill} task (difficulty ${difficulty})`
+  
+  return `A ${choose(TASK_DESCRIPTORS[type].adjectives)} ${choose(TASK_DESCRIPTORS[type].nouns)}`
 }
 
 export const TASK_TYPE = {
@@ -24,3 +31,62 @@ export const TASK_TYPES = Object.keys(TASK_TYPE) as TaskType[]
 export type TaskType = (typeof TASK_TYPE)[keyof typeof TASK_TYPE]
 
 export type Task = ReturnType<typeof newTask>
+
+// TODO: make descriptors for different base skills
+export const TASK_DESCRIPTORS = {
+  dungeon: {
+    adjectives: [
+      "dangerous",
+      "dreadful",
+      "horrid",
+      "terrible",
+      "tremendous",
+      "harrowing",
+      "deep",
+      "dark",
+      "gloomy",
+      "grim",
+      "vacuous",
+      "vile",
+    ],
+    nouns: [
+      "cave",
+      "cavern",
+      "crypt",
+      "delve",
+      "dungeon",
+      "grotto",
+      "labyrinth",
+      "maze",
+      "sepulcher",
+      "temple",
+      "vault",
+    ],
+  },
+  healing: {
+    adjectives: [
+      "healing",
+      "peaceful",
+      "refreshing",
+      "rejuventating",
+      "relaxing",
+      "restful",
+      "restorative",
+      "revitalizing",
+    ],
+    nouns: [
+      "bath",
+      "evening",
+      "meal",
+      "meditation",
+      "morning",
+      "recovery",
+      "relaxation",
+      "respite",
+      "rest",
+      "sleep",
+      "sojourn",
+      "treatment",
+    ],
+  },
+}
